24089cf6eb347f8319f6f4c058e8d6ecb1c14f02
Carsten Ruhl <ToxicGecko13@Gmail.com>
2019-04-10 22:56:00 -0700

    Sec Commit - 01 conveted to a Vue SPA

---
+++ 01 SPA/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>A Single Page App</h1>
+      <div id="nav">
+        <router-link to="/">Home Page</router-link>
+        <p><br></p>
+        <router-link to="/user">User Page</router-link>
+      </div>
+    </header>
+
+
+    <footer>
+      <em>(c) 2019 copyright <a href="https://mhintegrity.com">MHIntegrity</a></em>
+    </footer>
+    <router-view/>
+  </div>
+</template>
+
+
+<style scoped>
+  * {
+    box-sizing: border-box;
+    border: 0;
+    padding: 0;
+    margin: 0;
+  }
+  h1 {
+    font-size: 30px;
+    padding-bottom: 8px;
+  }
+  header {
+    position: sticky;
+    top: 0px;
+    background-color: lightsteelblue;
+    padding: 16px;
+  }
+  header nav {
+    list-style-type: none;
+  }
+  header nav {
+    margin: 6px;
+  }
+  footer {
+    position: fixed;
+    bottom: 0%;
+    background-color: lightsteelblue;
+    padding: 8px;
+    width: 100%;
+  }
+</style>
---
+++ 01 SPA/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+
+
+    <Articles/>
+
+
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Articles from '@/components/Articles.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Articles
+  }
+}
+</script>
+
+
+<style scoped>
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+</style>
